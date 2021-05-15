2fa282485396ac1f963caba55b925f5ee0849d9e
hiddro <edd.ckalb@gmail.com>
2021-05-15 18:20:34 -0500

    Cambios Cris Web

---
+++ index.js
@@ -0,0 +1,63 @@
+const toggle = document.querySelector('.toggle');
+const banner = document.querySelector('.banner');
+const marker = document.querySelector('#menu');
+const item = document.querySelectorAll('ul.menu li');
+// const url = window.location.pathname.split("/")[1];
+const url = window.location.pathname.split("/")[1].includes('semana') ? 'lab-2.html' : window.location.pathname.split("/")[1];
+var sObjeto;
+
+// console.log(marker);
+// console.log(item)
+// console.log(url)
+
+toggle.onclick = function (){
+    toggle.classList.toggle('active');
+    banner.classList.toggle('active');
+
+    if(sObjeto){
+        sObjeto.classList.toggle('activado');
+    }
+
+    if(url.includes('quiensoy')){
+        const imgYS = document.querySelector('#imgys');
+        const secQS = document.querySelector('#secqs');
+        imgYS.classList.toggle('addYoSoy');
+    }
+
+    if(url.includes('lab-2') || url.split('.')[0].includes('semana')){
+        const imgLab = document.querySelector('#imglab');
+        imgLab.classList.toggle('addYoSoy');
+    }
+
+    if(url.includes('contactame')){
+        const imgYS = document.querySelector('#imgys');
+        imgYS.classList.toggle('addYoSoy');
+    }
+}
+
+function parallax(e){
+    this.querySelectorAll('.elements').forEach(Layer => {
+        const speed = Layer.getAttribute('data-speed');
+        const x = (window.innerWidth - e.pageX*speed)/100;
+        Layer.style.transform = `translateX(${x}px)`;
+    });
+}
+
+function indicator(e){
+    // console.log(e);
+    e.classList.toggle('activado');
+}
+
+item.forEach(link => {
+
+    if(url.includes(link.id)){
+        sObjeto = link;
+    }
+
+    link.addEventListener('click', (e) => {
+        indicator(e.target);
+    });
+    
+});
+
+document.addEventListener("mousemove", parallax);
